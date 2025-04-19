dc85fc9fb10380079e4f6d9559fb09916f7e17e4
stanmoriasi <stanmoriasi@yahoo.com>
2025-04-19 13:34:02 -0600

    attempt to fix render build issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './services/auth.js';
-
+import { authenticateToken } from './utils/auth.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -17,6 +15,7 @@ const server = new ApolloServer({
   resolvers
 });
 
+
 const startApolloServer = async () => {
   await server.start();
   await db();
@@ -24,26 +23,28 @@ const startApolloServer = async () => {
   const PORT = process.env.PORT || 3001;
   const app = express();
 
-  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+
+  app.use('/images', express.static(path.join(__dirname, '../images/products')));
 
   app.use('/graphql', expressMiddleware(server as any,
     {
       context: authenticateToken as any
     }
   ));
+  
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${process.env.PORT}`);
+    console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
 };
